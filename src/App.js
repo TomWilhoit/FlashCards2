3944db81e6b117b2e4dbbd7a5d1a24ee24f04117
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-02-23 19:40:01 -0700

    Created local storage retrieval and dom rendering

--- src/App.js
+++ src/App.js
@@ -1,95 +1,104 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import CardContainer from './CardContainer';
-import Card from './Card';
-import mockData from './mockData.js';
-import './css/App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import CardContainer from "./CardContainer";
+import Card from "./Card";
+import mockData from "./mockData.js";
+import "./css/App.css";
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       questions: mockData.mockData,
       gameRestart: false,
+      repeatQuestions: true,
       questionIndex: 0,
       savedArray: [],
-      wrongArray:[],
-      currentElement: {}
-    }
+      wrongArray: []
+    };
   }
 
-
-  shouldRestartGame = () => {
-    if (this.state.gameRestart===false) {
+  componentWillMount() {
+    const localStorageArray = JSON.parse(
+      localStorage.getItem("savedQuestions")
+    );
+    console.log(localStorageArray);
     this.setState({
-      gameRestart: true
-    
-    })
-  } else{
-    this.setState({
-      gameRestart: false
-      })
-    }
+      wrongArray: localStorageArray
+    });
+    console.log(this.state.wrongArray);
   }
+
+  shouldRepeatQuestions = () => {
+    if (this.state.repeatQuestions === false) {
+      this.setState({
+        repeatQuestions: true
+      });
+    } else {
+      this.setState({
+        repeatQuestions: false
+      });
+    }
+  };
+
+  shouldRepeatQuestions = () => {
+    if (this.state.questionIndex === 55) {
+      this.setState({
+        repeatQuestions: true
+      });
+    }
+  };
 
   incrementQuestionIndex = () => {
     const newIndex = this.state.questionIndex + 1;
     this.setState({
       questionIndex: newIndex
-    })
-  }
+    });
+  };
 
   saveToStorage = () => {
-    let [...savedQuestions] = this.state.savedArray
-    savedQuestions.push(this.state.questions[this.state.questionIndex])
-    localStorage.setItem('savedQuestions',JSON.stringify(savedQuestions))
+    let [...savedQuestions] = this.state.savedArray;
+    savedQuestions.push(this.state.questions[this.state.questionIndex]);
+    localStorage.setItem("savedQuestions", JSON.stringify(savedQuestions));
     this.setState({
       savedArray: savedQuestions
-    })
-  }
+    });
+  };
 
-  pullFromStorage = () => {
-    return JSON.parse(localStorage.getItem('savedQuestions'))
-  }
+  renderWrongArray = () => {
+    console.log(this.state.wrongArray);
+    return this.state.wrongArray.map((element, index) => {
+      return (
+        <ul>
+          <li key={index}>{element.id}</li>
+          <li key={index}>{element.question}</li>
+          <li key={index}>{element.correctAnswer}</li>
+        </ul>
+      );
+    });
+  };
 
-  returnWrongArray(){
-    this.state.wrongArray.map(el => {
-      this.setState({
-        currentElement: el
-      })
-    })
-  }
-
-
-  
   render() {
-    if(this.state.gameRestart === false){
+    if (this.state.repeatQuestions === false) {
       return (
         <div className="App">
-          <Header/>
-          <CardContainer/>
+          <Header />
+          <CardContainer />
           <Card
-          questions={this.state.questions[this.state.questionIndex]}
-          questionIndex = {this.state.questionIndex}
-          incrementQuestionIndex={this.incrementQuestionIndex}/>
+            questions={this.state.questions[this.state.questionIndex]}
+            questionIndex={this.state.questionIndex}
+            incrementQuestionIndex={this.incrementQuestionIndex}
+            saveToStorage={this.saveToStorage}
+            shouldRepeatQuestions={this.shouldRepeatQuestions}
+          />
         </div>
-      )
-    }else{
-      this.returnWrongArray()
-    return (
-      <div className="wrong-questions-display">
-        <h1>Here's where we need some work:</h1>
-        <Card
-        id={this.state.currentElement.id}
-        questions={this.state.currentElement.question}
-        correctAnswer={this.state.currentElement.correctAnswer}
-        incrementQuestionIndex={this.incrementQuestionIndex}
-        saveToStorage={this.saveToStorage}
-        shouldRestartGame={this.shouldRestartGame}
-        />
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="App">
+          <div>{this.renderWrongArray()}</div>
+        </div>
+      );
     }
   }
 }
--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,26 @@ import React, { Component } from "react";
 import "./css/Card.css";
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correctClicked: false
+    };
+  }
+
+  falseGuess = () => {
+    alert("Wrong. Also don't use alert's because Pam will hate you.");
+    this.props.incrementQuestionIndex();
+    this.props.saveToStorage();
+  };
+
+  correctGuess = () => {
+    alert("Right! Also did you notice the typo in the last alert?");
+    this.props.incrementQuestionIndex();
+    this.props.shouldRepeatQuestions();
+  };
+
   render() {
-    console.log(this.props.questions)
     return (
       <div className="Card">
         <h2 className="question">{this.props.questions.question}</h2>
@@ -11,11 +29,11 @@ class Card extends Component {
           {this.props.questions.correctAnswer}
         </button>
         <button className="false-answer-btn1" onClick={this.falseGuess}>
-            {this.props.questions.falseAnswer1}
+          {this.props.questions.falseAnswer1}
         </button>
         <button className="false-answer-btn2" onClick={this.falseGuess}>
-            {this.props.questions.falseAnswer2}
-          </button>
+          {this.props.questions.falseAnswer2}
+        </button>
       </div>
     );
   }
